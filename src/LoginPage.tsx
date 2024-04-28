2c384dd3d5f0c17399ca37488cfb8ce19890e90e
Prashant Kamble <116736619+ANSHTRA@users.noreply.github.com>
2024-04-28 16:46:56 +0530

    initial commit

---
+++ src/LoginPage.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from "./authentication.module.css"
+
+export default function LoginPage() {
+    return (
+        <div className={styles.container}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <form action="" className={styles.form}>
+                <p>Login!</p>
+                <input type="text" placeholder="Username or Email" name="id" id="" />
+                <input type="password" placeholder="Password" name="password" id="" />
+                <input type="submit" value="login" />
+            </form>
+        </div >
+    )
+}
