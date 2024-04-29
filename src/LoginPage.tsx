b53f4ec1c3b4aaf7aeaee9a1aae060b8a69d9aaf
Prashant Kamble <116736619+ANSHTRA@users.noreply.github.com>
2024-04-29 11:26:58 +0530

    Added show password feature

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from "./authentication.module.css"
 
+
 export default function LoginPage() {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    function showPass() {
+        let el = inputRef.current;
+        if (el?.type === "password") {
+            el.type = "text";
+        } else if (el !== null) {
+            el.type = "password";
+        }
+    }
     return (
         <div className={styles.container}>
             <span></span>
@@ -25,7 +35,8 @@ export default function LoginPage() {
             <form action="" className={styles.form}>
                 <p>Login!</p>
                 <input type="text" placeholder="Username or Email" name="id" id="" />
-                <input type="password" placeholder="Password" name="password" id="" />
+                <input ref={inputRef} type="password" placeholder="Password" name="password" id="password" />
+                <input type="checkbox" onClick={showPass} name="" id="" />
                 <input type="submit" value="login" />
             </form>
         </div >
